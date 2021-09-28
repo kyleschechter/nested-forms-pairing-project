35c1263fc6d677322b178192d32dc2935a5aeadd
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-09-28 14:54:14 -0400

    done

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -9,8 +9,17 @@ class DoctorsController < ApplicationController
 
   def new
     @doctor = Doctor.new
+    2.times { @doctor.patients.build }
   end
 
   def create
+    @doctor = Doctor.create(doctor_params)
+    redirect_to doctor_path(@doctor)
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, patients_attributes: [:name, :age])
   end
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,4 @@
 class Doctor < ActiveRecord::Base
   has_many :patients
+  accepts_nested_attributes_for :patients  
 end
